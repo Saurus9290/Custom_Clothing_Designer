96557c2ada8620c0f65ddb493ccb27e0edbfcc55
vishnuvishwakarma <vv083150@gmail.com>
2023-11-26 00:26:39 +0530

    update indec.ts

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,104 @@
 import {
-    $query,
-    $update,
-    Record,
-    StableBTreeMap,
-    Vec,
-    match,
-    Result,
-    nat64,
-    ic,
-    Opt,
-    Principal,
-  } from "azle";
-  import { v4 as uuidv4 } from "uuid";
-  
+  $query,
+  $update,
+  Record,
+  StableBTreeMap,
+  Vec,
+  match,
+  Result,
+  nat64,
+  ic,
+  Opt,
+  Principal,
+} from "azle";
+import { v4 as uuidv4 } from "uuid";
 
-  type ClothingDesign = Record<{
-    id: string;
-    name: string;
-    description: string;
-    designer: Principal;
-    image: string;
-    createdAt: nat64;
-    updatedAt: Opt<nat64>;
-    sharedWith: Vec<Principal>;
-  }>;
-  
-    type ClothingDesignPayload = Record<{
-    name: string;
-    description: string;
-    image: string;
-  }>;
+// Define the Custom Clothing Design type
+type ClothingDesign = Record<{
+  id: string;
+  name: string;
+  description: string;
+  designer: Principal;
+  image: string;
+  createdAt: nat64;
+  updatedAt: Opt<nat64>;
+  sharedWith: Vec<Principal>;
+}>;
 
+// Define the Payload type for creating or updating clothing designs
+type ClothingDesignPayload = Record<{
+  name: string;
+  description: string;
+  image: string;
+}>;
+
+// Create a StableBTreeMap to store custom clothing designs
 const clothingDesignStorage = new StableBTreeMap<string, ClothingDesign>(0, 44, 1024);
 
-
-  $update;
-  export function createClothingDesign(payload: ClothingDesignPayload): Result<ClothingDesign, string> {
-    const clothingDesign: ClothingDesign = {
-      id: uuidv4(),
-      createdAt: ic.time(),
-      updatedAt: Opt.None,
-      designer: ic.caller(),
-      sharedWith: [],
-      ...payload,
-    };
-  
-    clothingDesignStorage.insert(clothingDesign.id, clothingDesign);
-    return Result.Ok<ClothingDesign, string>(clothingDesign);
+// Create a new custom clothing design
+$update;
+export function createClothingDesign(payload: ClothingDesignPayload): Result<ClothingDesign, string> {
+  // Payload Validation: Ensure that name, description, and image are present in the payload
+  if (!payload.name || !payload.description || !payload.image) {
+    return Result.Err("Missing required fields in the payload.");
   }
-  
 
-  $query;
-  export function getClothingDesign(id: string): Result<ClothingDesign, string> {
+  // Create a new clothing design record
+  const clothingDesign: ClothingDesign = {
+    id: uuidv4(),
+    createdAt: ic.time(),
+    updatedAt: Opt.None,
+    name: payload.name, // Explicit Property Setting
+    description: payload.description, // Explicit Property Setting
+    image: payload.image, // Explicit Property Setting
+    designer: ic.caller(),
+    sharedWith: [],
+  };
+
+  try {
+    clothingDesignStorage.insert(clothingDesign.id, clothingDesign); // Error Handling: Handle any errors during insertion
+  } catch (error) {
+    return Result.Err(`Failed to create the clothing design: ${error}`);
+  }
+
+  return Result.Ok<ClothingDesign, string>(clothingDesign);
+}
+
+// Get a custom clothing design by ID
+$query;
+export function getClothingDesignById(id: string): Result<ClothingDesign, string> {
+  // Validate Id Parameter
+  if (!id) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+  try {
     return match(clothingDesignStorage.get(id), {
       Some: (design) => Result.Ok<ClothingDesign, string>(design),
       None: () => Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} not found.`),
     });
+  } catch (err) {
+    return Result.Err<ClothingDesign, string>(`Error fetching Clothing Design with ID=${id}: ${err}`);
   }
-  
-$query;
-export function getAllClothingDesigns(): Result<Vec<ClothingDesign>, string> {
-  return Result.Ok(clothingDesignStorage.values());
 }
 
+// Get all custom clothing designs
+$query;
+export function getAllClothingDesigns(): Result<Vec<ClothingDesign>, string> {
+  try {
+    return Result.Ok(clothingDesignStorage.values());
+  } catch (err) {
+    return Result.Err<Vec<ClothingDesign>, string>(`Error fetching all Clothing Designs: ${err}`);
+  }
+}
+
+// Update a custom clothing design
 $update;
 export function updateClothingDesign(id: string, payload: ClothingDesignPayload): Result<ClothingDesign, string> {
+  // Validate Id Parameter
+  if (!id) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+
   return match(clothingDesignStorage.get(id), {
     Some: (existingDesign) => {
       const updatedDesign: ClothingDesign = {
@@ -80,8 +114,14 @@ export function updateClothingDesign(id: string, payload: ClothingDesignPayload)
   });
 }
 
+// Delete a custom clothing design by ID
 $update;
 export function deleteClothingDesign(id: string): Result<ClothingDesign, string> {
+  // Validate Id Parameter
+  if (!id) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+
   return match(clothingDesignStorage.get(id), {
     Some: (existingDesign) => {
       clothingDesignStorage.remove(id);
@@ -91,51 +131,66 @@ export function deleteClothingDesign(id: string): Result<ClothingDesign, string>
   });
 }
 
-
-  $update;
-  export function shareClothingDesign(id: string, user: Principal): Result<ClothingDesign, string> {
-    return match(clothingDesignStorage.get(id), {
-      Some: (design) => {
-        if (!design.sharedWith.includes(user)) {
-          design.sharedWith.push(user);
-          clothingDesignStorage.insert(id, design);
-          return Result.Ok<ClothingDesign, string>(design);
-        }
-        return Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} is already shared with the user.`);
-      },
-      None: () => Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} not found.`),
-    });
+// Share a custom clothing design with a user
+$update;
+export function shareClothingDesign(id: string, user: Principal): Result<ClothingDesign, string> {
+  // Validate Id Parameter
+  if (!id) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
   }
-  
-  $update;
-  export function unshareClothingDesign(id: string, user: Principal): Result<ClothingDesign, string> {
-    return match(clothingDesignStorage.get(id), {
-      Some: (design) => {
-        if (design.sharedWith.includes(user)) {
-          design.sharedWith = design.sharedWith.filter((sharedUser) => sharedUser !== user);
-          clothingDesignStorage.insert(id, design);
-          return Result.Ok<ClothingDesign, string>(design);
-        }
-        return Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} is not shared with the user.`);
-      },
-      None: () => Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} not found.`),
-    });
-  }
-  
-  
 
-  globalThis.crypto = {
-    //@ts-ignore
-    getRandomValues: () => {
-      let array = new Uint8Array(32);
-  
-      for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
+  if (!user) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+
+  return match(clothingDesignStorage.get(id), {
+    Some: (design) => {
+      if (!design.sharedWith.includes(user)) {
+        design.sharedWith.push(user);
+        clothingDesignStorage.insert(id, design);
+        return Result.Ok<ClothingDesign, string>(design);
       }
-  
-      return array;
+      return Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} is already shared with the user.`);
     },
-  };
-  
+    None: () => Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} not found.`),
+  });
+}
 
+// Remove sharing of a custom clothing design from a user
+$update;
+export function unshareClothingDesign(id: string, user: Principal): Result<ClothingDesign, string> {
+  // Validate Id Parameter
+  if (!id) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+
+  if (!user) {
+    return Result.Err<ClothingDesign, string>('Invalid ID format');
+  }
+
+  return match(clothingDesignStorage.get(id), {
+    Some: (design) => {
+      if (design.sharedWith.includes(user)) {
+        design.sharedWith = design.sharedWith.filter((sharedUser) => sharedUser !== user);
+        clothingDesignStorage.insert(id, design);
+        return Result.Ok<ClothingDesign, string>(design);
+      }
+      return Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} is not shared with the user.`);
+    },
+    None: () => Result.Err<ClothingDesign, string>(`Clothing Design with ID=${id} not found.`),
+  });
+}
+
+globalThis.crypto = {
+  //@ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+
+    return array;
+  },
+};
 
